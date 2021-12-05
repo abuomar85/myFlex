103432ec6374f7cc2b9e7a8c7e0aee0ae2c56b18
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2021-12-05 21:00:56 +0100

    update the files index and documentation
    adding id to the movie
    write the code to send
    GET a list of movies
    DELETE a movie by id
    PUT a new movie

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
 // importing libraries  Use the Morgan middleware library to log all requests 
-const express = require('express');
+const express = require('express'),
+bodyParser = require('body-parser'),
+  uuid = require('uuid');
 const app = express(),
       morgan = require('morgan');
       app.use(morgan('common'));
+      app.use(bodyParser.json());
 
 // list of top ten movies 
-let topMovies = [
+let movies = [
   {
-    title: 'Braveheart',
+    id: 1,
+    name: 'Braveheart',
     staring: 'Mel Gibson',
     dirctor: 'Mel Gibson'
   },
   {
-    title: 'Titanic',
+    id: 2,
+    name: 'Titanic',
     staring: 'Leonardo DiCaprio',
     dirctor: 'James Cameron'
   },
   {
-    title: 'The Italian Job',
+    id: 3,
+    name: 'The Italian Job',
     staring: 'Mark Wahlberg',
     dirctor: 'F. Gary Gray'
   },
   {
-    title: 'Al-Zeer Salem',
+    id: 4,
+    name: 'Al-Zeer Salem',
     staring: 'Salloum Haddad',
     dirctor: 'Hatem Ali'
   },
   {
-    title: 'Money Heist',
+    id: 5,
+    name: 'Money Heist',
     staring: 'Álvaro Morte',
     dirctor: 'Álex Pina'
   },
   {
-    title: ' Death Note',
+    id: 6,
+    name: ' Death Note',
     staring: 'Takeshi Obata',
     dirctor: 'Tsugumi Ohba'
   },
   {
-    title: 'Friends',
+    id: 7,
+    name: 'Friends',
     staring: 'David Crane',
     dirctor: 'David Crane'
   },
   {
-    title: 'The Fresh Prince of Bel-Air',
+    id: 8,
+    name: 'The Fresh Prince of Bel-Air',
     staring: 'Will Smith',
     dirctor: 'Andy Borowitz'
   },
   {
-    title: 'Bad Boys II',
+    id: 9,
+    name: 'Bad Boys II',
     staring: 'Will Smith',
     dirctor: 'Michael Bay'
   },
   {
-    title: 'Spider-Man 2',
+    id: 10,
+    name: 'Spider-Man 2',
     staring: 'Tobey Maguire',
     dirctor: 'Sam Raimi'
   },
@@ -66,9 +79,36 @@ app.get('/', (req, res) => {
 app.get('/documentation', (req, res) => {                  
   res.sendFile('public/documentation.html', { root: __dirname });
 });
-
+// Get a list of all movies 
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
+});
+// Get the data about a single movie, by name 
+app.get('/movies/:name', (req, res) => {
+  res.json(movies.find((movie) =>
+    { return movie.name === req.params.name }));
+});
+
+// Adds data for a new movie to the  list of movies.
+app.post('/movies', (req,res)=> {
+  let newMovie = req.body;
+  if(!newMovie.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newMovie.id = uuid.v4(); 
+    movies.push(newMovie);
+    res.status(201).send(newMovie);
+  }
+}); 
+// Deletes a  movie from our list by ID
+app.delete('/movies/:id', (req, res) => {
+  let movie = movies.find((movie) => { return movie.id === req.params.id });
+
+  if (movie) {
+    movies = movies.filter((obj) => { return obj.id !== req.params.id });
+    res.status(201).send('Movie ' + req.params.id + ' was deleted.');
+  }
 });
 
 // create myLogger  
